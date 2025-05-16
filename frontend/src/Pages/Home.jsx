1643ddb6f1e9d861d6d6e3c272bd4ccabb318f5f
da-nn-yy <db7394928@gmail.com>
2025-05-16 17:04:12 +0300

    feat: add Tailwind CSS utilities for container and nav-link styles

--- frontend/src/Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <div className="text-2xl mt-10 max-lg:8xl max-sm:text-8xl">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page of our application.</p>
-        </div>
-    );
-}
-export default Home;
