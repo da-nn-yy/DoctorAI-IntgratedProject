31ca302e5aabf3e651df64ae0672580fe98996eb
da-nn-yy <db7394928@gmail.com>
2025-05-17 15:50:51 +0300

    feat: add About route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import './App.css'
 import Home from './Components/Home'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import About from './Components/About';
+
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Navbar/>
         <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
         </Routes>
       </Router>
     </>
